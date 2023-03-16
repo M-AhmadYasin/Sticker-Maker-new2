89942f8c87163e90927fe36ee8ff653e58430b0d
Personal <mahmad.56627@gmail.com>
2023-03-16 19:45:23 +0500

    Commit 1.0

--- app/src/main/java/com/pro/stickermaker/MainActivity.kt
+++ app/src/main/java/com/pro/stickermaker/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.pro.stickermaker
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.pro.stickermaker.databinding.ActivityMainBinding
 
@@ -31,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        Log.d("TAG", "onCreate: ")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
